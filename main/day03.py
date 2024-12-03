a665639e2f67f05a63f27b68786e49a97a955a05
Accieo <42718776+Accieo@users.noreply.github.com>
2024-12-03 20:34:44 +0100

    Solve day 3

--- main/day03.py
+++ main/day03.py
@@ -1,16 +1,24 @@
 import re
 import time
-from typing import Literal
+from typing import Literal, List, Tuple
 
 def common(input_source: Literal["input", "examples"] = "input"):
     with open(f"{input_source}/day03.txt", "r") as file:
         data = file.read().strip()
-        matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", data)
 
-    return matches
+    return data
+
+def parse_mul(data: str) -> List[Tuple[str, str]]:
+    MUL_ONLY = r"mul\((\d{1,3}),(\d{1,3})\)"
+    return re.findall(MUL_ONLY, data)
+
+def parse_all(data: str) -> List[Tuple[str, str, str]]:
+    MUL_AND_CONDITIONALS = r"(mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\))"
+    return re.findall(MUL_AND_CONDITIONALS, data)
 
 def part_one(input_source: Literal["input", "examples"] = "input") -> int:
-    operations = common(input_source)
+    data = common(input_source)
+    operations = parse_mul(data)
 
     products = []
     for l, r in operations:
@@ -19,7 +27,21 @@ def part_one(input_source: Literal["input", "examples"] = "input") -> int:
     return sum(products)
 
 def part_two(input_source: Literal["input", "examples"] = "input"):
-    return
+    data = common(input_source)
+    operations = parse_all(data)
+
+    products = []
+    enabled = True
+    for op, l, r in operations:
+        if op == "don't()":
+            enabled = False
+        elif op == "do()":
+            enabled = True
+        elif op.startswith("mul("):
+            if enabled:
+                products.append(int(l) * int(r))
+
+    return sum(products)
 
 if __name__ == "__main__":
     start_one = time.perf_counter()
