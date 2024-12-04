855fbc111d0c5a3713f6385864a2d2e8d6d11abc
Accieo <42718776+Accieo@users.noreply.github.com>
2024-12-04 20:15:07 +0100

    Solve day 4 part 2

--- main/day04.py
+++ main/day04.py
@@ -1,5 +1,5 @@
 import time
-from typing import Literal, List
+from typing import Literal, List, Tuple
 
 def common(input_source: Literal["input", "examples"] = "input"):
     with open(f"{input_source}/day04.txt", "r") as file:
@@ -58,12 +58,58 @@ def xmas_count(word_sample: List[str]) -> int:
 
     return total_count
 
+def get_neighbors(x: int, y: int, max_length: int) -> List[Tuple[int, int]]:
+    """
+    Gets neighbouring coordinates in a matrix.
+    """
+    # diffs = [down, up, left, right, down-right, up-right, down-left, up-left]
+    diffs = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    neighbors = list(map(lambda diff: (x + diff[0], y + diff[1]), diffs))
+    neighbors = list(filter(lambda coord: (0 <= coord[0] < max_length) and (0 <= coord[1] < max_length), neighbors))
+
+    return neighbors
+
+def mas_count(word_sample: List[str]) -> int:
+    """
+    Counts occurrences of `MAS` spelled in an X shape.
+    e.g.
+    S S  M M
+     A    A
+    M M  S S
+    """
+    total_count = 0
+    target_words = ["SAM", "MAS"]
+    # Find all A
+    coords_a = []
+    for n, line in enumerate(word_sample):
+        if "A" not in line:
+            continue
+
+        for m, ch in enumerate(line):
+            if ch == "A":
+                coords_a.append((n, m))
+
+    for y, x in coords_a:
+        neighbors = get_neighbors(x=x, y=y, max_length=len(word_sample))
+        if len(neighbors) == 8:
+            dl = word_sample[neighbors[6][1]][neighbors[6][0]]
+            ur = word_sample[neighbors[5][1]][neighbors[5][0]]
+            joined_forward = "".join([dl, word_sample[y][x], ur])
+            ul = word_sample[neighbors[7][1]][neighbors[7][0]]
+            dr = word_sample[neighbors[4][1]][neighbors[4][0]]
+            joined_backward = "".join([ul, word_sample[y][x], dr])
+            if joined_forward in target_words and joined_backward in target_words:
+                total_count += 1
+
+    return total_count
+
 def part_one(input_source: Literal["input", "examples"] = "input"):
     word_sample = common(input_source)
     return xmas_count(word_sample)
 
 def part_two(input_source: Literal["input", "examples"] = "input"):
-    return
+    word_sample = common(input_source)
+    return mas_count(word_sample)
 
 if __name__ == "__main__":
     start_one = time.perf_counter()
--- tests/day04.py
+++ tests/day04.py
@@ -1,13 +1,13 @@
 import unittest
 
-from main.day04 import part_one, part_two, xmas_count
+from main.day04 import part_one, part_two
 
 class Day04Tests(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual(part_one(input_source="examples"), 18)
 
     def test_part_two(self):
-        pass
+        self.assertEqual(part_two(input_source="examples"), 9)
 
 if __name__ == "__main__":
     unittest.main()
